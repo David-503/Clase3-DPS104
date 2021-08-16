9244bc674b4899c9c5a0470299942bff71e38a31
David Bonilla <david.bonilla625@gmail.com>
2021-08-16 12:47:14 -0600

    Desarrollo de la clase 3

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const estilos = StyleSheet.create({
+    grandeAzul: {
+        color: "#6A7EBB",
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+    negro: {
+        color: "black",
+        fontWeight: "bold",
+        fontSize: 17,
+    },
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image
+                style={{ width: 700, height: 350, resizeMode: "stretch" }}
+                source={require("./assets/img/programacion.jpg")}
+            />
+            <Text style={estilos.grandeAzul}>David Alejandro Bonilla </Text>
+            <Text style={estilos.negro}> BA181927 </Text>
+
+            <Text style={estilos.negro}>
+                Ingeniería en Ciencias de la Computación
+            </Text>
+            <Text style={estilos.negro}> C# </Text>
+            <Text style={[estilos.negro]}>
+                Baloncesto, Video Juegos, Viajes
+            </Text>
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
